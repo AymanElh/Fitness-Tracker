ddcfa347d5fe6a8d3a7d15f98bfb70846007fcc2
Ayman Elh <elhamioui1964@gmail.com>
2025-05-04 17:57:18 +0100

    fix an issue of null images make a default image

--- resources/views/frontoffice/pages/foods.blade.php
+++ resources/views/frontoffice/pages/foods.blade.php
@@ -64,7 +64,7 @@
                          data-brand="{{ strtolower($food->brand ?? '') }}">
                         <!-- Food Image -->
                         <div class="h-48 overflow-hidden">
-                            <img src="{{ $food->image_url ?? 'https://via.placeholder.com/400x300?text=No+Image' }}"
+                            <img src="{{ $food->image_url ?? 'https://images.unsplash.com/photo-1592545287571-8afd34aada57?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}"
                                  alt="{{ $food->name }}"
                                  class="w-full h-full object-cover transition duration-300 transform hover:scale-105">
                         </div>
